df7e49d68dfad3bdbd27944b04e9d6d07b71a36d
Basant kumar Hota <basahota@users.noreply.github.com>
2023-04-24 18:55:25 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/spring/batch/entity/Customer.java
@@ -0,0 +1,38 @@
+package com.javatechie.spring.batch.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "CUSTOMERS_INFO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @Column(name = "CUSTOMER_ID")
+    private int id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "GENDER")
+    private String gender;
+    @Column(name = "CONTACT")
+    private String contactNo;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "DOB")
+    private String dob;
+
+
+}
